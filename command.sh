f974b49a2b57537f59e476f9b9968d87df9a0812
opsgauraw <126002803+opsgauraw@users.noreply.github.com>
2023-08-24 00:28:31 +0530

    Create command.sh

---
+++ command.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Command 1
+ls -lrt
+# Add your command 1 here
+
+# Command 2
+ls -pqrwgdhstsh
+# Add your command 2 here
+
+# Command 3
+curl -IsSf www.google.com | head -n 1
+# Add your command 3 here
